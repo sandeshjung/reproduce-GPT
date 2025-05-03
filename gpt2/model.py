ad52a29172d8272a4f6e79ba5235d74ea8bae12f
Jung <sandeshjung@gmail.com>
2025-05-03 12:20:24 +0545

    feat: added initialization block and residual initialization

--- gpt2/model.py
+++ gpt2/model.py
@@ -23,6 +23,7 @@ class CasualSelfAttention(nn.Module):
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
         # regularization
         self.n_head = config.n_head
         self.n_embd = config.n_embd
@@ -62,6 +63,7 @@ class MLP(nn.Module):
         # in gpt2 paper, the traditional way of defining GELU is used i.e. approximate argument 'tanh'
         self.gelu = nn.GELU(approximate="tanh")
         self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+        self.c_proj.NANOGPT_SCALE_INIT = 1
 
     def forward(self, x):
         x = self.c_fc(x)
@@ -100,6 +102,24 @@ class GPT(nn.Module):
             ))
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
+        # weight sharing between TE and output layer
+        self.transformer.wte.weight = self.lm_head.weight
+
+        # initialize weights
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            std = 0.02
+            if hasattr(module, 'NANOGPT_SCALE_INIT'):
+                std  *= (2 * self.config.n_layer) ** -0.5
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=0.02)
+
+        
     def forward(self, idx, targets=None):
         B, T = idx.size() # shape (B, T)
         assert (
--- gpt2/train.py
+++ gpt2/train.py
@@ -5,22 +5,26 @@ from model import GPT, GPTConfig
 from dataloader import DataLoader
 
 def train():
-     # initialize the model.
-     device = 'cpu'
-     model = GPT(GPTConfig())
-     model.to(device)
- 
-     train_loader = DataLoader(B=4, T=32)
- 
-     optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
-     for i in range(50):
-         x, y = train_loader.next_batch()
-         x, y = x.to(device), y.to(device)
-         optimizer.zero_grad()
-         _, loss = model(x, y)
-         loss.backward()
-         optimizer.step()
-         print(f"step: {i}, loss: {loss.item()}")
+    # initialize the model.
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = GPT(GPTConfig())
+    model.to(device)
+
+    torch.manual_seed(1337)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(1337)
+
+    train_loader = DataLoader(B=4, T=32)
+
+    optimizer = torch.optim.AdamW(model.parameters(), lr=3e-4)
+    for i in range(50):
+        x, y = train_loader.next_batch()
+        x, y = x.to(device), y.to(device)
+        optimizer.zero_grad()
+        _, loss = model(x, y)
+        loss.backward()
+        optimizer.step()
+        print(f"step: {i}, loss: {loss.item()}")
  
  
 if __name__ == "__main__":
